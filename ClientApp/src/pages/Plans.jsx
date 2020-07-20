3f5d5de65472789a1c79dec69982c3b77fcd601f
MarcusDLG <delag010@gmail.com>
2020-07-20 11:03:47 -0400

    pseudo-code for plans page

--- ClientApp/src/pages/Plans.jsx
+++ ClientApp/src/pages/Plans.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header'
 
 export default function Plans() {
   return (
-    <div>
-      <h1>Plans</h1>
-    </div>
+    <>
+      <Header />
+      {/* div to contain the main content
+          sub-component called to fill out the menu on the left and the content for each heading on the left. 
+      */}
+    </>
   )
 }
