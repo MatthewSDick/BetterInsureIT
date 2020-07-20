e38a7d16ffaa012a2c17fa7b0dc6162080544426
MarcusDLG <delag010@gmail.com>
2020-07-20 11:11:26 -0400

    structuring components for sub-menus

---
+++ ClientApp/src/components/CustomerPlans.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomerPlans = () => {
+  return (
+    <>
+      <aside></aside>
+    </>
+  )
+}
+
+export default CustomerPlans
--- ClientApp/src/pages/Plans.jsx
+++ ClientApp/src/pages/Plans.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
+import Footer from '../components/Footer'
+import CustomerPlans from '../components/CustomerPlans'
 
 export default function Plans() {
   return (
     <>
       <Header />
-      {/* div to contain the main content
-          sub-component called to fill out the menu on the left and the content for each heading on the left. 
-      */}
+      <CustomerPlans></CustomerPlans>
+      {/* div to contain the main content*/}
+      {/* component called to fill out the menu on the left and the content for each heading on the left.  */}
+      <Footer />
     </>
   )
 }
