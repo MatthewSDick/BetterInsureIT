9f3bf84fb152a18f21389bb18a32cff78da1ac47
MarcusDLG <delag010@gmail.com>
2020-07-22 10:56:32 -0400

    Basic subplan structure

---
+++ ClientApp/src/components/SubPlanContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SubPlanContent = () => {
+  return (
+    <>
+      <div className="plan-information"></div>
+      <h2>Life Insurance</h2>
+    </>
+  )
+}
+
+export default SubPlanContent
